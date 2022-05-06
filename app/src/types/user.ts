2f1d55d3eacf44371ca10295a691e1949215a5ff
Rico Sandyca <ricosandyca.if@gmail.com>
2022-05-06 14:54:36 +0700

    Fix user document types

--- app/src/types/user.ts
+++ app/src/types/user.ts
@@ -2,10 +2,25 @@ import firestore from 'firebase/firestore';
 
 export const USERS_COLLECTION = 'users';
 
+export type UserWorkExperience = {
+  id: string;
+  job_title: string;
+  company: string;
+  company_logo?: string;
+  job_description?: string;
+  start_date: firestore.Timestamp;
+  end_date?: firestore.Timestamp;
+};
+
 export type User = {
   uid: string;
-  name: string | null;
-  email: string | null;
+  email: string;
+  key: string; // unique user key to define their vanity URL
+  is_private: boolean; // to determine is the user's profile is viewable to other users
+  name: string;
+  profile_picture?: string;
+  age?: number;
+  work_experiences: UserWorkExperience[];
   created_at: firestore.Timestamp;
   updated_at: firestore.Timestamp;
 };
