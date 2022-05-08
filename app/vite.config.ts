149cccb17def92020a3c541a814ad5fcc1e6f529
Rico Sandyca <ricosandyca.if@gmail.com>
2022-05-08 08:58:55 +0700

    Add vite PWA plugin

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,10 +2,11 @@ import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin({ cache: false })],
+  plugins: [react(), eslintPlugin(), VitePWA()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
